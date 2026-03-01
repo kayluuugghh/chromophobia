609d8a078117b438ca86295632edab312f631497
kayluuugghh <90522796+kayluuugghh@users.noreply.github.com>
2026-03-01 01:05:07 -0600

    switching framework from ASP.NET to Node.JS/REACT

---
+++ src/Login.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './Login.css'
+
+// function requestAuthorization() {
+
+// }
+
+function Login() {
+  return (
+    <>
+      <h1>Chromophobia</h1>
+      <div>
+        <button onClick={(requestAuthorization)}>Login</button>
+      </div>
+    </>
+  )
+}
+
+export default Login
