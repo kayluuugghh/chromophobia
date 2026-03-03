6b854460ee3e4dbe52aff6f1aef0448b47d6524e
drishyaregmi <rdrishya13@gmail.com>
2026-03-03 01:02:22 -0600

    Corrected Spotify show-dialog param and use .env vars
    - Fixed show-dialodue to show_dialog
    - Use .env vars for Spotify client ID and redirect URI

--- src/utils/spotifyAuth.js
+++ src/utils/spotifyAuth.js
@@ -1,5 +1,5 @@
 const clientId = import.meta.env.VITE_CLIENT_ID;
-const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+// const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 const redirectURI = import.meta.env.VITE_REDIRECT_URI;
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
 
@@ -8,7 +8,7 @@ export function requestAuthorization() {
     url += "?client_id=" + clientId;
     url += "&response_type=code";
     url += "&redirect_uri=" + encodeURIComponent(redirectURI);
-    url += "&show_dialogue=true";
+    url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming"
     window.location.href = url;
 }
