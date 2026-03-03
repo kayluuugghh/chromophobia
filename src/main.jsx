c89f0c3bce62589fe1e46deb36f13543b707ec82
drishyaregmi <rdrishya13@gmail.com>
2026-03-03 00:55:16 -0600

    Added routing for login, home and team pages.
    Redirects to home page after successful login.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import Login from './Login.jsx'
+
+import Login from './Login.jsx';
+import Home from './Home.jsx';
+import Team from './Team.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/callback" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
