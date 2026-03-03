3d6bcc2cf2a9f8ac5c195c0db6f1ab9ca681ada6
drishyaregmi <rdrishya13@gmail.com>
2026-03-03 00:42:09 -0600

    Implemented navigation bar and Spotify handling.
    Hamburher menu drops down vertically with menu items.
    Home and Teams menu items use React Router to navigate to Home and Team pages.
    About menu iten is a link that opens transparent background with about information and a close button.
    Customization menu item is a placeholder for future customization options.
    Login page navigates to homepage after successful Spotify authentication.

---
+++ src/assets/Navbar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { BsBrush } from "react-icons/bs";
+import './Navbar.css';
+import { Link } from "react-router-dom";
+
+function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isAboutOpen, setIsAboutOpen] = useState(false); // modal state
+    const toggleMenu = () => {setIsOpen(!isOpen);};
+    const handleCustomization = () => {alert("Customization options coming soon.");};
+    const toggleAbout = () => {setIsAboutOpen(!isAboutOpen);};
+
+    return (
+        <>
+        <nav className="navbar">
+
+            {/* 3 stacked bars to create hamburger icon */}
+            <div className= "hamburger" onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+
+            {/* navigation links  */}
+            <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+                <li><Link to="/home">Home</Link></li>
+                <li><a onClick={toggleAbout} className="link-btn">About</a></li>
+                <li><Link to="/team">Team</Link></li>
+            </ul>
+            
+            {/* Customization button */}
+            <button onClick={handleCustomization} className="customize-btn"><BsBrush className="customize-icon" /></button>
+
+        </nav>
+        {/* About Modal */}
+        {isAboutOpen && (
+            <div className="modal-overlay" onClick={toggleAbout}>
+                <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                    <h2>About Chromophobia</h2>
+                    <p>*Explain here*</p>
+                    <button onClick={toggleAbout} className="close-btn">X</button>
+                </div>
+            </div>
+        )}
+        </>
+    );
+}
+
+export default NavBar;
