95c03ede2d9c59112da68677eaa75dba6ccc1a83
drishyaregmi <rdrishya13@gmail.com>
2026-03-03 01:05:39 -0600

    feat(UI): Created home and team pages.

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+
+import NavBar from './assets/Navbar.jsx';
+
+function Home() {
+    return (
+        <>
+            <NavBar />
+            <div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
