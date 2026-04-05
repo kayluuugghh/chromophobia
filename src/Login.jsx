d1f22cf66c357017e54494e50825d3e3efce17cc
kayluuugghh <90522796+kayluuugghh@users.noreply.github.com>
2026-04-05 18:21:40 -0500

    fix: re-added cursor following feature

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,6 +3,7 @@ import {useNavigate} from 'react-router-dom';
 import { loginWithSpotify } from './utils/spotifyAuth.js'
 import NavBar from './assets/Navbar.jsx';
 import './Login.css'
+import Follower from './assets/Follower.jsx'
 
 function Login() {
     const navigate = useNavigate();
@@ -17,6 +18,7 @@ function Login() {
     return (
         <>
             <h1>Chromophobia</h1>
+            <Follower></Follower>
             <button onClick={loginWithSpotify} className="login-button">
                 Login with Spotify
             </button>
