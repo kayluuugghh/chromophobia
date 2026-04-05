2bf75a1f0627b6d9d5aa015991e46e6a98bd614c
drishyaregmi <rdrishya13@gmail.com>
2026-04-04 23:54:16 -0500

    feat: added cat follower component

---
+++ src/assets/Follower.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import pixel from './pixel.png';
+import './Follower.css';
+
+function Follower() {
+    const [pixelPosition, setPixelPosition] = useState({ x: 0, y: 0 });
+    const [facingLeft, setFacingLeft] = useState(false);
+
+    useEffect(() => {
+        let lastX = 0;
+
+        function movePixel(e) {
+            setPixelPosition({ x: e.clientX, y: e.clientY }); 
+
+            if (e.clientX < lastX) {
+                setFacingLeft(true);
+            } else if (e.clientX > lastX) {
+                setFacingLeft(false);
+            }
+            lastX = e.clientX;
+        };
+
+        window.addEventListener('mousemove', movePixel);
+        return () => {
+            window.removeEventListener('mousemove', movePixel);
+        };
+    }, []);
+
+    return (
+        <img 
+            src={pixel} 
+            alt="Cat" 
+            className="pixel"
+            style={{ 
+                left: pixelPosition.x + 'px', 
+                top: pixelPosition.y + 'px',
+                transform: facingLeft
+                    ? 'translate(-50%, -50%) scaleX(-1)'
+                    : 'translate(-50%, -50%) scaleX(1)',
+            }}
+        />
+    );
+}
+
+export default Follower;
