185fcde9bbf46d956408d08558eef5a6e3feb87e
drishyaregmi <rdrishya13@gmail.com>
2026-04-15 21:48:17 -0500

    styling changes

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import {useNavigate} from 'react-router-dom';
 import { loginWithSpotify } from './utils/spotifyAuth.js'
-import NavBar from './assets/Navbar.jsx';
+// import NavBar from './assets/Navbar.jsx';
 import './Login.css'
 import Follower from './assets/Follower.jsx'
 
@@ -17,10 +17,10 @@ function Login() {
 
     return (
         <>
-            <h1>CHROMOPHOBIA</h1>
+            <h1 className='title'>Chromophobia</h1>
             <Follower></Follower>
             <button onClick={loginWithSpotify} className="login-button">
-                Login with Spotify
+                Log in with Spotify
             </button>
         </>
     );
